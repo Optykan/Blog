2be6e5505902f05103b7d19f9b039aa232052b6c
Optykan <syang0525@gmail.com>
2017-11-18 18:39:32 -0500

    streamline server process

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,32 @@
+var fs          = require('fs');
 var gulp        = require('gulp');
+var child       = require('child_process');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
-    browserSync.init({
-        proxy: "localhost:3000"
-    });
 
-    gulp.watch("assets/scss/*.scss", ['sass']);
-    gulp.watch(["routes/*.html", "views/*.ejs"]).on('change', browserSync.reload);
+gulp.task('server', ['sass'], function() {
+	var server = child.spawn('node', ['bin/www']);
+	var log = fs.createWriteStream('server.log', {flags: 'a'});
+	server.stderr.pipe(log);
+
+	browserSync.init({
+		proxy: "localhost:3000",
+		port: 3001
+	});
+
+	gulp.watch("assets/scss/*.scss", ['sass']);
+	gulp.watch(["routes/*.html", "views/*.ejs"]).on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("assets/scss/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("public/stylesheets"))
-        .pipe(browserSync.stream());
+	return gulp.src("assets/scss/*.scss")
+	.pipe(sass({ outputStyle: 'compressed' })
+		.on('error', sass.logError))
+	.pipe(gulp.dest("public/stylesheets"))
+	.pipe(browserSync.stream());
 });
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['server']);
