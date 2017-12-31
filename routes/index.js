a91a4b6b307e4f3f24b51b6646058a697bf2e906
Optykan <syang0525@gmail.com>
2017-12-31 16:19:26 -0500

    reflow landing page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Express' });
+	res.render('index', { 
+		title: 'Express',
+		projects: [{
+			title: 'Avante Landscaping',
+			image: '/images/portfolio/avante-1.jpg',
+			description: 'some words here',
+			link: 'https://avantelandscaping.com'	
+		},{
+			title: 'Unionville Skating Club',
+			image: 'http://via.placeholder.com/300x300',
+			description: 'more words here',
+			link: 'https://unionvilleskatingclub.com'
+		},{
+			title: 'Project 5K',
+			image: 'http://via.placeholder.com/300x300',
+			description: 'even more words',
+			link: 'https://project5k.ca'
+		}	
+		]
+	});
 });
 
 module.exports = router;
