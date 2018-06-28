5925684684b181a83e250a3032d5518da7363d60
Optykan <syang0525@gmail.com>
2018-06-28 18:16:40 -0400

    forgot a library

--- gulpfile.js
+++ gulpfile.js
@@ -66,7 +66,7 @@ gulp.task('js', function(){
 
 gulp.task('compressHomeJs', function(cb){
 	pump([
-        gulp.src(['public/javascripts/vendor/jquery.js', 'public/javascripts/vendor/foundation.min.js', 'public/javascripts/app.js', 'public/javascripts/home.js', 'public/javascripts/client-auth.js']),
+        gulp.src(['public/javascripts/vendor/jquery.js', 'public/javascripts/vendor/wow.min.js', 'public/javascripts/vendor/foundation.min.js', 'public/javascripts/app.js', 'public/javascripts/home.js', 'public/javascripts/client-auth.js']),
         concat('bundle-home.js'),
         uglify(),
         gulp.dest('public/javascripts/')
