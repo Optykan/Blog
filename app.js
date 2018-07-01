3c38581cdfe8aa9fc1787d72909cdac45e46bc7f
Optykan <syang0525@gmail.com>
2018-07-01 15:53:38 -0400

    install helmet

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var helmet = require('helmet');
 
 var fs = require('fs');
 var index = require('./routes/index')
@@ -43,6 +44,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
 
 app.use('/', index);
 app.use('/blog', blog);
