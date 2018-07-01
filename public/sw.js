c06bb2ed893ede7afdb58b74a819ee4f81eec7e6
Optykan <syang0525@gmail.com>
2018-07-01 10:52:30 -0400

    fixed theme color

--- public/sw.js
+++ public/sw.js
@@ -9,7 +9,26 @@ self.addEventListener('install', function (e) {
 });
 
 self.addEventListener('fetch', function (event) {
-  event.respondWith(caches.match(event.request).then(function (response) {
-    return response || fetch(event.request);
-  }));
+	evt.respondWith(fromNetwork(evt.request, 400).catch(function () {
+	    return fromCache(evt.request);
+	}));
 });
+
+function fromCache(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
+
+function fromNetwork(request, timeout) {
+  return new Promise(function (fulfill, reject) {
+    var timeoutId = setTimeout(reject, timeout);
+ 
+    fetch(request).then(function (response) {
+      clearTimeout(timeoutId);
+      fulfill(response); 
+    }, reject);
+  });
+}
