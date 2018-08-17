1a8c0fbf15658dfdc04f2eb6e19d0d4035f57f31
Optykan <syang0525@gmail.com>
2018-08-16 22:43:23 -0400

    i changed something

--- routes/blog.js
+++ routes/blog.js
@@ -1,59 +1,31 @@
 var express = require('express');
 var router = express.Router();
-
+const fetch = require("node-fetch");
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.render('blog', { 
 		title: 'Blog | Samuel Yang',
 		page: 'blog',
-		projects: [{
-			title: 'Avante Landscaping',
-			subtitle: 'WordPress•Foundation6',
-			image: '/images/portfolio/avante-1.jpg',
-			description: 'Solo project developed in conjunction with the weonderful team at nvision. Built on the WordPress platform.',
-			link: 'https://avantelandscaping.com'	
-		},{
-			title: 'Unionville Skating Club',
-			subtitle: 'WordPress•Foundation6',
-			image: '/images/portfolio/usc-1.jpg',
-			description: 'The second of two projects developed at nvision. Quite the massive project, so it was a team project.',
-			link: 'https://unionvilleskatingclub.com'
-		},{
-			title: 'Tzuyu',
-			subtitle: 'Node.js',
-			image: 'https://via.placeholder.com/300x300',
-			description: 'IA discord bot built on discord.js that supports playing music, permission management (ish), and rendering LaTeX. It also has support for user-developed plugins.',
-			link: 'https://github.com/Optykan/Tzuyu'
-		},{
-			title: 'Beat',
-			subtitle: 'MongoDB•Angular•Ionic',
-			image: '/images/portfolio/beat-1.jpg',
-			description: 'PennApps XVII Top 10! Based on the all-too-popular Pitch Perfect riff-off concept.',
-			link: 'https://github.com/kpsuperplane/pennapps-beat/' 
-		},{
-			title: 'TeachAssist Scraper',
-			subtitle: 'PHP',
-			image: '/images/portfolio/teachassist-1.jpg',
-			description: 'Using 100% legitimate and approved methods, it attempts to scrape your marks from YRDSB TeachAssist. Basically it just looks better.',
-			link: 'https://github.com/Optykan/TeachAssist'
-		},{
-			title: 'Boilerplate',
-			subtitle: 'Various',
-			image: 'https://via.placeholder.com/300x300',
-			description: 'React + Foundation6 + Sass + Gulp.js + Browserify + Express.js + Babel makes for easy site setup. Just clone it and get started!',
-			link: 'https://github.com/Optykan/site-boilerplate'
-		},{
-			title: 'Blog',
-			subtitle: 'Node.js•Express•Foundation 6',
-			image: '/images/portfolio/blog-1.jpg',
-			description: 'Well, you\'re literally looking at it. Built from scratch based on my boilerplate.',
-			link: 'https://github.com/Optykan/Blog'
-		}
-		]
 	});
 });
 
-router.get('/post', function(req, res, next){
+router.get('/post/:id', function(req, res, next){
+	let origin = req.get("host")
+	let postId = req.params.id
+	let url = req.protocol + '://' + origin + '/api/posts/'+postId
+
+	fetch(url).then(body=>{
+		if(body.status === Response.STATUS_NO_CONTENT){
+			throw new Error('Page not found')
+		} else {
+			return body.json()
+		}
+	}).then(json=>{
+		let data = json.response
+		res.render('blog-page', { title: title })
+	}).catch(e=>{
+		console.error(e)
+	})
 	res.render('blog-page', { title: 'Some Post' });
 });
 
