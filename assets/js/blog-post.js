4d7b56954305f1be305c4e030679c3c1fef6c9ec
Optykan <syang0525@gmail.com>
2018-08-23 21:17:26 -0400

    hopefully this fixes it

--- assets/js/blog-post.js
+++ assets/js/blog-post.js
@@ -2,9 +2,9 @@
 	console.log("blog")
 	let postContainer = document.getElementById("blog-post-content");
 	let postContent = postContainer.innerHTML;
-	postContent = postContent.replace(/\&gt;/gi, '>').replace(/\&lt;/gi, '<');
+	postContent = postContent.replace(/\&gt;/gi, '>').replace(/\&lt;/gi, '<').replace(/\n/g, '\n\n');
 	
-	// postContainer.innerHTML = marked(postContent);
+	postContainer.innerHTML = marked(postContent);
 
 	hljs.initHighlightingOnLoad();
 
--- public/javascripts/blog-post.js
+++ public/javascripts/blog-post.js
@@ -4,9 +4,9 @@
 	console.log("blog");
 	var postContainer = document.getElementById("blog-post-content");
 	var postContent = postContainer.innerHTML;
-	postContent = postContent.replace(/\&gt;/gi, '>').replace(/\&lt;/gi, '<');
+	postContent = postContent.replace(/\&gt;/gi, '>').replace(/\&lt;/gi, '<').replace(/\n/g, '\n\n');
 
-	// postContainer.innerHTML = marked(postContent);
+	postContainer.innerHTML = marked(postContent);
 
 	hljs.initHighlightingOnLoad();
 
